1934b4d793c0b79d7b27258cc7ca1d9e909a9472
MCV <ktuce.mcanv@gmail.com>
2022-10-30 02:06:23 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,45 @@
+from operator import index
+import streamlit as st
+# import plotly.express as px
+from pycaret.regression import setup, compare_models, pull, save_model, load_model
+import pandas_profiling
+import pandas as pd
+from streamlit_pandas_profiling import st_profile_report
+import os
+
+if os.path.exists('./dataset.csv'):
+    df = pd.read_csv('dataset.csv', index_col=None)
+
+with st.sidebar:
+    st.image("https://media-exp1.licdn.com/dms/image/C5612AQGbh-2GzkdqAQ/article-cover_image-shrink_600_2000/0/1590327785584?e=2147483647&v=beta&t=uDqRCy7_1WlUTkhsvyWCuoiryH1__fVIq6ral2K1ttk")
+    st.title("AutoMLOps")
+    choice = st.radio("Navigation", ["Upload", "Profiling", "Modelling", "Download"])
+    st.info("This app make explore own dataset and train ML algorithms")
+
+if choice == "Upload":
+    st.title("Upload Your Dataset")
+    file = st.file_uploader("Upload Your Dataset")
+    if file:
+        df = pd.read_csv(file, index_col=None)
+        df.to_csv('dataset.csv', index=None)
+        st.dataframe(df)
+
+if choice == "Profiling":
+    st.title("Exploratory Data Analysis")
+    profile_df = df.profile_report()
+    st_profile_report(profile_df)
+
+if choice == "Modelling":
+    chosen_target = st.selectbox('Choose the Target Column', df.columns)
+    if st.button('Run Modelling'):
+        setup(df, target=chosen_target, silent=True)
+        setup_df = pull()
+        st.dataframe(setup_df)
+        best_model = compare_models()
+        compare_df = pull()
+        st.dataframe(compare_df)
+        save_model(best_model, 'best_model')
+
+if choice == "Download":
+    with open('best_model.pkl', 'rb') as f:
+        st.download_button('Download Model', f, file_name="best_model.pkl")
